5b3ce2b8543d59e4cd928e73fff86712bb185fe7
Clivern <hello@clivern.com>
2023-07-18 23:53:12 +0200

    wip

--- lua/teacup.lua
+++ lua/teacup.lua
@@ -2,26 +2,23 @@
 local mod = require("teacup.module")
 
 ---@class Config
----@field opt string Your config option
 local config = {
   opt = "Hello!",
 }
 
----@class MyModule
 local M = {}
 
 ---@type Config
 M.config = config
 
----@param args Config?
--- you can define your setup function here. Usually configurations can be merged, accepting outside params and
--- you can also put some validation here for those.
+-- Setup method
 M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
+-- Hello method
 M.hello = function()
-  return module.great(M.config.opt)
+  return mod.great(M.config.opt)
 end
 
 return M
